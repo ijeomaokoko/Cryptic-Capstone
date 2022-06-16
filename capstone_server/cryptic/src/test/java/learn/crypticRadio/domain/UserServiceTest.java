042541380f5843e1784962a5897788997cf8ef55
Ijeoma Okoko <ijeomaokoko@yahoo.com>
2022-06-16 10:10:52 -0500

    update

---
+++ capstone_server/cryptic/src/test/java/learn/crypticRadio/domain/UserServiceTest.java
@@ -0,0 +1,13 @@
+package learn.crypticRadio.domain;
+import learn.crypticRadio.models.AppUser;
+import learn.crypticRadio.models.Message;
+import learn.crypticRadio.domain.MessageService;
+import learn.crypticRadio.models.Room;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+
+public class UserServiceTest {
+
+}
