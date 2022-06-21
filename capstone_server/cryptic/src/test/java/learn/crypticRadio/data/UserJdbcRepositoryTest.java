ff0616c44ddaf439a8a498ba5f40d2bd59597540
Ijeoma Okoko <ijeomaokoko@yahoo.com>
2022-06-21 09:15:04 -0500

    update

--- capstone_server/cryptic/src/test/java/learn/crypticRadio/data/UserJdbcRepositoryTest.java
+++ capstone_server/cryptic/src/test/java/learn/crypticRadio/data/UserJdbcRepositoryTest.java
@@ -3,8 +3,10 @@ package learn.crypticRadio.data;
 import learn.crypticRadio.models.AppUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.NotExtensible;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
 
@@ -35,10 +37,25 @@ class UserJdbcRepositoryTest {
 
     @Test
     void add() {
+        AppUser user = makeJohn();
+//        User actual = repository.add(user);
+//        assertNotNull(actual);
+       // assertEquals(, actual.getUsername());
+
+        user= makeJohn();
+        User actual = repository.add(user);
+        assertNotNull(actual);
+        assertEquals("john@smith.com", actual.getUsername());
     }
 
     @Test
     void update() {
+        User user = makeJohn();
+        user.getUsername();
+        assertFalse(repository.update(makeJohn()));
+        user.getUsername();
+        assertFalse(repository.update(makeJohn()));
+
     }
 
     private AppUser makeJohn(){
--- capstone_server/cryptic/src/test/java/learn/crypticRadio/domain/RoomServiceTest.java
+++ capstone_server/cryptic/src/test/java/learn/crypticRadio/domain/RoomServiceTest.java
@@ -1,4 +1,6 @@
 package learn.crypticRadio.domain;
+import learn.crypticRadio.data.MessageJdbcRepository;
+import learn.crypticRadio.data.RoomJdbcRepository;
 import learn.crypticRadio.models.AppUser;
 import learn.crypticRadio.models.Role;
 import learn.crypticRadio.domain.RoomService;
@@ -17,6 +19,69 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 
 public class RoomServiceTest {
+    @MockBean
+    RoomJdbcRepository repository;
+
+    @Autowired
+    RoomService service;
+
+
+    @Test
+    void shouldAddWhenValid(){
+        Room expected = new Room();
+        Room arg = new Room();
+        arg.setRoomId(0);
+
+        when(repository.add(arg)).thenReturn(expected);
+        Result<Room> result = service.add(arg);
+        assertEquals(ResultType.SUCCESS, result.getType());
+
+        assertEquals(expected, result.getPayload());
+    }
+
+    @Test
+    void shouldNotAddWhenInvalid() {
+        Room room = makeRoom();
+        Result<Room> result = service.add(room);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        room.setRoomId(0);
+        room.setRoomName(null);
+        result = service.add(room);
+        assertEquals(ResultType.INVALID, result.getType());
+
+    }
+
+    @Test
+    void shouldUpdate () {
+        Room room = new Room();
+        room.setRoomId(1);
+        room.setRoomName("");
+
+        when(repository.update(room)).thenReturn(true);
+
+        Result<Room> actual = service.update(room);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdate () {
+        Room room = new Room();
+        room.setRoomId(1);
+        when(repository.update(room)).thenReturn(true);
+
+        Result<Room> actual = service.update(room);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+
+
+    private Room makeRoom() {
+        Room room = new Room();
+        room.setRoomId(1);
+        room.setRoomName("room name not found.");
+        return room;
+    }
+
 
 
 
