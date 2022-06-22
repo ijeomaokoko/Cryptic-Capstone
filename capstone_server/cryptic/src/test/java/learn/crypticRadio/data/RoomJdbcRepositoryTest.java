3d773e78f3cdf0ec9009b984a6121e14b4aa6ccd
Josephmac <joseph-0@live.com>
2022-06-22 14:39:35 -0500

    room test stuff

--- capstone_server/cryptic/src/test/java/learn/crypticRadio/data/RoomJdbcRepositoryTest.java
+++ capstone_server/cryptic/src/test/java/learn/crypticRadio/data/RoomJdbcRepositoryTest.java
@@ -1,29 +1,56 @@
 package learn.crypticRadio.data;
 
+import learn.crypticRadio.models.Room;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class RoomJdbcRepositoryTest {
 
+    @Autowired
+    RoomJdbcRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
     @Test
     void findByUserId() {
+        List<Room> found = repository.findByUserId(1);
+        assertNotNull(found);
+        assertEquals(2,found.size());
+
+        found = repository.findByUserId(2);
+        assertNotNull(found);
+        assertEquals(0,found.size());
     }
 
     @Test
     void findByRoomId() {
+
     }
 
     @Test
     void add() {
+
     }
 
     @Test
     void update() {
+
     }
 
     @Test
     void deleteByRoomId() {
+
     }
 }
